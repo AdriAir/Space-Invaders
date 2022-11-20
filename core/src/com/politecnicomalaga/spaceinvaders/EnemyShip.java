a6df3f337d97abcb2946ecc9ba20cbd12f0c85bf
AdriAir <aborio2004@gmail.com>
2022-11-20 20:41:56 +0100

    EnemyShip

--- core/src/com/politecnicomalaga/spaceinvaders/EnemyShip.java
+++ core/src/com/politecnicomalaga/spaceinvaders/EnemyShip.java
@@ -1,4 +1,29 @@
 package com.politecnicomalaga.spaceinvaders;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class EnemyShip {
+    private int x;
+    private int y;
+    private Texture img;
+    private int speed;
+
+    public EnemyShip(){
+        img = new Texture("naveEnemiga.png");
+        x = (Gdx.graphics.getWidth() / 2) - (img.getWidth() / 2);
+        y = 10;
+        speed = 1;
+    }
+
+    public void draw(SpriteBatch batch){
+        batch.draw(img, x, y);
+    }
+
+    public void dispose(){
+        if (img != null) {
+            img.dispose();
+        }
+    }
 }
