a0c1d4708bcfca111cb0c273deeee9ddd0583d31
AdriAir <aborio2004@gmail.com>
2022-12-06 10:20:03 +0100

    Clase principal mejorada. Contiene contador de pasos para manejar el tiempo, escuadron, listas de balas enemigas y amigas. Se está desarrollando una mejora para mostrar varias naves enemeigas de forma ordenada.

--- core/src/com/politecnicomalaga/spaceinvaders/GDXSpaceInvaders.java
+++ core/src/com/politecnicomalaga/spaceinvaders/GDXSpaceInvaders.java
@@ -6,10 +6,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+
 public class GDXSpaceInvaders extends ApplicationAdapter {
 	SpriteBatch batch;
 	FriendlyShip friendlyShip;
-	EnemyShip enemyShip;
+	ArrayList<EnemyShip> squadron;
+	ArrayList<EnemyShot> enemyBullets;
+	ArrayList<FriendlyShot> friendlyBullets;
 	int pasos;
 	Texture img;
 
@@ -17,21 +21,43 @@ public class GDXSpaceInvaders extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		friendlyShip = new FriendlyShip();
-		enemyShip = new EnemyShip();
-		pasos = 0;
+		squadron = new ArrayList<EnemyShip>();
+		enemyBullets = new ArrayList<EnemyShot>();
+		friendlyBullets = new ArrayList<FriendlyShot>();
+		pasos = 1;
 		img = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
 
+		pasos++;
+
+		if (pasos == 30){
+			//Han pasados 500ms
+
+		} else if(pasos == 60){
+			//Ha pasado 1 segundo
+
+			pasos = 1;
+		}
+
 		ScreenUtils.clear(0.18f, 0.48f, 0.96f, 0);
+
 		batch.begin();
 		batch.draw(img,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		friendlyShip.draw(batch);
-		friendlyShip.moverse();
-		enemyShip.draw(batch);
+
+		for (EnemyShip myEnemyShip: squadron) {
+
+			myEnemyShip.draw(batch);
+			
+		}
+		
 		batch.end();
+
+		friendlyShip.moverse();
+
 	}
 
 	@Override
@@ -39,6 +65,21 @@ public class GDXSpaceInvaders extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 		friendlyShip.dispose();
-		enemyShip.dispose();
+
+		for (EnemyShip myEnemyShip: squadron) {
+
+			myEnemyShip.dispose();
+
+		}
+		for (EnemyShot myEnemyBullet: enemyBullets) {
+
+			//myEnemyBullet.dispose();
+
+		}
+		for (FriendlyShot myFriendlyBullet: friendlyBullets) {
+
+			//myFriendlyBullet.dispose();
+
+		}
 	}
 }
