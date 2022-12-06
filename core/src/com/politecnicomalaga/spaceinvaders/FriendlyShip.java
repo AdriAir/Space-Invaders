073ea7b707bf0007de6df28e069f2a3f98932de7
Noelia Segovia <noesegsan@gmail.com>
2022-12-06 11:07:02 +0100

    eliminado movimiento de la nave en esta clase para poder pasarlo a la principal

--- core/src/com/politecnicomalaga/spaceinvaders/FriendlyShip.java
+++ core/src/com/politecnicomalaga/spaceinvaders/FriendlyShip.java
@@ -14,50 +14,25 @@
         private Texture img;
 
         private float speedX;
-        private float speedY;
 
-        private float direccion;
+        private float direction;
         //Constructor
         public FriendlyShip(){
             img = new Texture("nave.png");
             x = 0.9f + (Gdx.graphics.getWidth() / 2) - (img.getWidth() / 2);
             y = 0f;
-            speedX = 2.0f;
-            speedY = 0.5f;
-            direccion = speedX;
+            speedX = 5.0f;
+            direction = speedX;
         }
 
         //RESTO DE MÉTODO
         //Movimientos
-        public void moverse() {
-
-            if (Gdx.input.justTouched()) {
-                //OBTENER X donde se toca
-                float posPulsadaX = Gdx.input.getX();
-
-                if (posPulsadaX <= Gdx.graphics.getWidth() / 2) {//Si la posicion cuando pulsen es <= a la mitad del ancho de gráfico....
-                    direccion = speedX; //para un lado
-                } else if (posPulsadaX >= Gdx.graphics.getWidth() / 2) {
-                    direccion = -speedX; //para otro
-                }
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT) ){
-                direccion = -speedX; //para un lado
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) ){
-                direccion = speedX; //para un lado
-            }
-
-            if (x >= Gdx.graphics.getWidth() - 100) {
-                direccion = -speedX;
-            } else if (x <= 0) {
-                direccion = speedX;
-            }
-
-            x = x + direccion;
-
-
+        public void moverse(float speedX) {
+            this.speedX = speedX;
+            direction = speedX;
+            x = x+ direction;
         }
+
         //dibujarse
         public void draw(SpriteBatch batch){
             batch.draw(img, x, y, 100, 100);
@@ -69,5 +44,4 @@
                 img.dispose();
             }
         }
-
     }
